106f9b8e9179e5fd9de9b45342fc142cfc7e60db
gigamosh57 <gigamosh57@gmail.com>
2014-09-15 17:52:31 -0600

    Update Project_1_Prob4.py

--- Project_1_Prob4.py
+++ Project_1_Prob4.py
@@ -7,10 +7,75 @@ import matplotlib.pyplot as plt
 
 dxdt = lambda x : -x
 x0 = 1
-dt = 0.9
+
+dt = 0.1
 t = np.arange(0,20,dt)
 xnow = x0
+x_01 = ()
 for a in t:
 	xnext = xnow + dxdt(xnow)*dt
 	xnow = xnext
-	print xnext
+	x_01 = x_01 + (xnow,)
+
+dt = 0.5
+t = np.arange(0,20,dt)
+xnow = x0
+x_05 = ()
+for a in t:
+	xnext = xnow + dxdt(xnow)*dt
+	xnow = xnext
+	x_05 = x_05 + (xnow,)
+
+dt = 1
+t = np.arange(0,20,dt)
+xnow = x0
+x_1 = ()
+for a in t:
+	xnext = xnow + dxdt(xnow)*dt
+	xnow = xnext
+	x_1 = x_1 + (xnow,)
+
+dt = 2
+t = np.arange(0,20,dt)
+xnow = x0
+x_2 = ()
+for a in t:
+	xnext = xnow + dxdt(xnow)*dt
+	xnow = xnext
+	x_2 = x_2 + (xnow,)
+
+dt = 2.5
+t = np.arange(0,20,dt)
+xnow = x0
+x_25 = ()
+for a in t:
+	xnext = xnow + dxdt(xnow)*dt
+	xnow = xnext
+	x_25 = x_25 + (xnow,)
+
+v = x_01	
+xplot = np.arange(0,len(v),1)/float(len(v))
+plt.plot(xplot,v,color = "b")
+
+v = x_05	
+xplot = np.arange(0,len(v),1)/float(len(v))
+plt.plot(xplot,v,color = "g")
+
+v = x_1
+xplot = np.arange(0,len(v),1)/float(len(v))
+plt.plot(xplot,v,color = "r")
+
+v = x_2
+xplot = np.arange(0,len(v),1)/float(len(v))
+plt.plot(xplot,v,color = "c")
+
+v = x_25
+xplot = np.arange(0,len(v),1)/float(len(v))
+plt.plot(xplot,v,color = "m")
+
+plt.show()
+
+### This plot show s
+
+
+

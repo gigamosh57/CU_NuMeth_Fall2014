53bfe0b1847d756c8981df3fbedeedbd010a8a40
gigamosh57 <gigamosh57@gmail.com>
2014-09-15 10:50:08 -0600

    Update Project_1_Prob2.py

--- Project_1_Prob2.py
+++ Project_1_Prob2.py
@@ -34,24 +34,36 @@ fprime = lambda x : 3*x**2 + 3*np.sin(3*x)
 ### Solve with initial guess
 guess = 2
 tol = 0.000000001
-n = 1000
+n = 50
 it, xroot, f_xroot = mynewton(guess, n, tol)
 print "iterations: " + np.str(it)
+#### prints the following:
+#### "iterations: 6"
 print "first root lower than: " + np.str(guess) + " is " + np.str(xroot)
+#### prints the following:
+#### "first root lower than: 2 is 0.48539431657"
 print "at this root, the function evaluates as: " + np.str(f_xroot)
+#### prints the following:
+#### "at this root, the function evaluates as: -8.32667268469e-17"
 
 
 # Define second Function
 f = lambda x : x*np.tan(x)-3
 # Fprime
-fprime = lambda x : np.tan(x) + x*np.arccos(x)
+fprime = lambda x : np.tan(x) + x*(1/(np.cos(x)))**2
 
 ### Solve with initial guess
-guess = 2
+guess = 1.4
 tol = 0.000000001
-n = 1000
+n = 50
 it, xroot, f_xroot = mynewton(guess, n, tol)
 print "iterations: " + np.str(it)
+#### prints the following:
+#### "iterations: 6"
 print "first root lower than: " + np.str(guess) + " is " + np.str(xroot)
+#### prints the following:
+#### "first root lower than: 1.4 is 1.19245882934"
 print "at this root, the function evaluates as: " + np.str(f_xroot)
+#### prints the following:
+#### "at this root, the function evaluates as: -4.4408920985e-16"
 

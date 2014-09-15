d26a5e3d7f8ae9c9a11a84951e22c516934dc20a
gigamosh57 <gigamosh57@gmail.com>
2014-09-15 10:29:50 -0600

    Update Project_1_Prob2.py

--- Project_1_Prob2.py
+++ Project_1_Prob2.py
@@ -26,23 +26,32 @@ def mynewton(guess,n = 1000,tol = 0.01):
 
 
 
-# Define Function
+# Define First Function
 f = lambda x : x**3 - np.cos(3*x)
 # Fprime
 fprime = lambda x : 3*x**2 + 3*np.sin(3*x)
 
-
-### TEST SOLUTION FOR SINGLE VALUE
+### Solve with initial guess
 guess = 2
 tol = 0.000000001
-n = 50
+n = 1000
 it, xroot, f_xroot = mynewton(guess, n, tol)
-
 print "iterations: " + np.str(it)
 print "first root lower than: " + np.str(guess) + " is " + np.str(xroot)
-#### this prints
-#### "first root lower than: 2 is 0.48539431657"
 print "at this root, the function evaluates as: " + np.str(f_xroot)
-#### this prints
-#### "at this root, the function evaluates as: -8.32667268469e-17"
+
+
+# Define second Function
+f = lambda x : x*np.tan(x)-3
+# Fprime
+fprime = lambda x : np.tan(x) + x*np.arccos(x)
+
+### Solve with initial guess
+guess = 2
+tol = 0.000000001
+n = 1000
+it, xroot, f_xroot = mynewton(guess, n, tol)
+print "iterations: " + np.str(it)
+print "first root lower than: " + np.str(guess) + " is " + np.str(xroot)
+print "at this root, the function evaluates as: " + np.str(f_xroot)
 

03c4363edf3ff9d5c80f80f6c66c2e78d8924bc7
gigamosh57 <gigamosh57@gmail.com>
2014-10-18 00:01:53 -0600

    Update Project_2_Prob2.py

--- Project_2_Prob2.py
+++ Project_2_Prob2.py
@@ -1,13 +1,109 @@
 ####################################################################
  ######## Runge-Kutta ODE solver with Cash-Karp 4th-5th order params
- 
-#def pagerkck4(feval,x0,tstart,tfinal,dt,ord = 4,tol=10**-6):
 
-#### Solver code goes here
+def pagerkck4(feval,x0,tstart,tfinal,dt,order = 4,tol=10**-2,errtol = 10**-20):
+   # INITIALIZE ARRAY OF CASH-KARP FACTORS
+   ck_a = np.array([[0],[1/5.],[3/10.],[3/5.],[1.],[7/8.]])
+   ck_b = np.array([[0,0,0,0,0],
+               [1/5.,0,0,0,0],
+               [3/40.,9/40.,0,0,0],
+               [3/10.,-9/10.,6/5.,0,0],
+               [-11/54.,5/2.,-70/27.,35/27.,0],
+               [1631/55296.,175/512.,575/13824.,44275/110592.,253/4096.]])
+   ck_c =  np.array([[37/378.],[0.],[250/621.],[125/594.],[0.],[512/1771.]])
+   ck_cs = np.array([[2825/27648.],[0.],[18575/48384.],[13525/55296.],[277/14336.],[1/4.]])
+   
+   ck_a = ck_a[range(0,order),:]
+   ck_b = ck_b[range(0,order),][:,range(0,order)]
+   ck_c = ck_c[range(0,order),:]
+   ck_cs = ck_cs[range(0,order),:]
+   
+   #initialize time vector
+   tvec=[tstart]
+   #initialize x, initialize solution matrix xsol
+   x0 = np.array([x0]).T
+   x=x0
+   xsol = np.empty((len(x0),0),float)
+   xsol = np.append(xsol,x0,axis=1)
+   ts = [0]
+   
+   # SINCE THIS FUNCTION USES ADAPTIVE TIMESTEPPING
+   # THE TIME VECTOR IS NOT DEFINED INITIALLY
+   t = tstart
+   h = dt
+   errvec = []
+   while t < tfinal:
+      
+      ## RESET VALUES
+      error = tol*1.01
+      xs = []
+      xn = []
+      hvec = []
+      
+      xnvec = []
+      xsvec = []
+      errloop = 0
+      while errloop == 0:
+         # EVALUATE STAGES
+         f = np.empty((0,1),float)
+         for fn in feval:
+            f = np.append(f,fn(x,t))
+         
+         k = np.array([h*f]).T
+         
+         # LOOPS THROUGH ALL K VALUES DEPENDING ON ORDER OF FUNCTION
+         for klev in range(1,order):
+            # CALCULATES F FOR ALL FUNCTIONS IN FEVAL
+            f = np.empty((0,1),float)
+            # EVALUATES F DEPENDING ON LEVEL OF K
+            for fn in feval:
+               cb = np.array([ck_b[klev,range(0,klev)]])
+               ca = ck_a[klev]
+               kev = k[:,range(0,klev)]
+               f = np.append(f,fn(x+np.array([np.sum(cb*kev,axis=1)]).T,t+ca*h))
+            
+            f = np.array([f]).T
+            k = np.append(k,h*f,axis = 1)
+            # End for klev
+         
+         # CALCULATE x values
+         xnew = np.sum(x + np.array([np.sum(ck_c.T*k,axis = 1)]).T,axis = 1)
+         xs = np.sum(x + np.array([np.sum(ck_cs.T*k,axis=1)]).T , axis = 1)
+         
+         # ESTIMATE ERROR
+         abserror = np.absolute(xnew-xs)
+         relerror = np.zeros((2,1))
+         for i in range(0,len(xnew)): 
+            if xnew[i] > errtol: relerror[i] = np.absolute((xnew[i] - xs[i])/xnew[i])
+         
+         error = np.amax(np.amax(abserror),np.amax(relerror))
+         
+         # ADJUST H BASED ON ERROR
+         hnew = h*(tol/error)**(0.2)
+         hvec = hvec + [hnew]
+         if hnew > h*1.2: hnew=h*1.2
+         h = hnew
+         if error < tol: errloop = 1
+         ### End while error > tol
+      
+      h = hnew
+      # STORE VALUES FROM ADAPTIVE TIMESTEPPING
+      x = np.reshape(xnew,(-1,1))
+      
+      # STORE SOLUTION (we assume XNEW is better)
+      xsol = np.append(xsol,x,axis = 1)
+      # ADJUST TIME BY H
+      t = t + h 
+      tvec = tvec + [t]
+      ts = ts + [h]
+      
+      ### End while time < tfinal
+      
+   return tvec,xsol,ts
 
-#   return tvec,xsol,ts
 ######## Solver ends here
 ################################################
+
 
 
 ################################################
@@ -28,15 +124,28 @@ feval = [lambda x,t: a*x[0,0]-c*x[0,0]*x[1,0],
       lambda x,t: d*x[0,0]*x[1,0]-b*x[1,0]]
 x0 =  [100,10]
 tstart = 0
-tfinal = 100
-dt = (tfinal-tstart)/10000.
-order = 4
+tfinal = 10
+dt = (tfinal-tstart)/1000.
+order = 3
+tol=10**-2
+errtol = 10**-20
 
-#tvec,xsol,ts = pagerkck4(feval,x0,tstart,tfinal,dt,ord = 4,tol=10**-6)
+######## End function input here
+################################################
+
+################################################
+######## test solver with function here:
+
+
+tvecf,xsolf,tsf = pagerkck4(feval,x0,tstart,tfinal,dt,order = 3)
+
+######## End solver function input here
+################################################
 
 ################################################
 ######## test solver without function here
-   # INITIALIZE ARRAY OF CASH-KARP FACTORS
+
+# INITIALIZE ARRAY OF CASH-KARP FACTORS
 ck_a = np.array([[0],[1/5.],[3/10.],[3/5.],[1.],[7/8.]])
 ck_b = np.array([[0,0,0,0,0],
             [1/5.,0,0,0,0],
@@ -63,8 +172,6 @@ ts = [0]
 
 # SINCE THIS FUNCTION USES ADAPTIVE TIMESTEPPING
 # THE TIME VECTOR IS NOT DEFINED INITIALLY
-tol=10**-3
-errtol = 10**-20
 t = tstart
 h = dt
 
@@ -121,18 +228,13 @@ while t < tfinal:# and it1 < 20:
       xnew = np.sum(x + np.array([np.sum(ck_c.T*k,axis = 1)]).T,axis = 1)
       xs = np.sum(x + np.array([np.sum(ck_cs.T*k,axis=1)]).T , axis = 1)
       
-      xnvec = np.append(xnvec,xnew)
-      xsvec = np.append(xsvec,xs)
-      
       # ESTIMATE ERROR
       abserror = np.absolute(xnew-xs)
       relerror = np.zeros((2,1))
       for i in range(0,len(xnew)): 
          if xnew[i] > errtol: relerror[i] = np.absolute((xnew[i] - xs[i])/xnew[i])
       
-      #error = np.amin([np.absolute(np.amax(tol/abserror)),np.absolute(np.amax(tol/relerror))])
       error = np.amax(np.amax(abserror),np.amax(relerror))
-      #error  = np.amax(np.absolute(xnew-xs))
       
       # ADJUST H BASED ON ERROR
       hnew = h*(tol/error)**(0.2)
@@ -179,21 +281,16 @@ while t < tfinal:# and it1 < 20:
 ######## Solver ends here
 ################################################
 
+#plt.plot(tvec,xsol1)
+#plt.plot(tvec,xsol2)
+#plt.show() 
 
 
-pt = range(1,len(tvec)-1)
-tvec = [tvec[i] for i in pt]
-ts = [ts[i] for i in pt]
-xsol1 = [xsol[0,i] for i in pt]
-xsol2 = [xsol[1,i] for i in pt]
-#errvec = [errvec[i] for i in pt]
-plt.plot(tvec,xsol1)
-plt.plot(tvec,xsol2)
+
+
+
+plt.plot(xsol[0,:],xsolf[0,:])
+plt.plot(xsol[1,:],-1*xsolf[1,:])
 plt.show() 
-
-
-#plt.plot(tvec,xsol[0,:])
-#plt.plot(tvec,xsol[1,:])
-#plt.show()
 
 

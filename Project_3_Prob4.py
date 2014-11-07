1f55625c0871df7d90e245a62bd9ebc3a2bd92d6
gigamosh57 <gigamosh57@gmail.com>
2014-11-06 20:21:24 -0700

    Update Project_3_Prob4.py

--- Project_3_Prob4.py
+++ Project_3_Prob4.py
@@ -19,11 +19,6 @@ def thomas(a,b,c,r):
    y = b*0
    J = len(b)
    
-   # r
-   r[0] = c1*x[0] + c2*(x[1]-x[0])/deltax
-   r[1:(nx-1)] = x[1:(nx-1)]
-   r[nx-1] = c3*x[0] + c4*(x[J-1]-x[J-2])/deltax
-   
    # define efg matrix
    f[0] = b[0]
    g[0] = c[0]/f[0]
@@ -51,7 +46,7 @@ def thomas(a,b,c,r):
 fig = plt.figure()
 axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 axes.set_xlabel('x (L)')
-axes.set_ylabel('T')
+axes.set_ylabel('Head (ft)')
 ######## End Initialize figure
 ################################################
 
@@ -60,18 +55,30 @@ axes.set_ylabel('T')
 ######## Setup Problem Part a
 
 # Variable definition
-L = 1.0
-b = 1.0
+
+T = 400
+S = 0.01
+b = T/S
 h0 = 2
 t0 = 1
+# so the distance is far enough for BCs to not interfere
+L = 5*np.sqrt(T*t0/S)
 
 # Grid definition
-deltax = 0.01
+deltax = 0.5
 nx = int(L/deltax+1)
-deltat = 0.5*np.sqrt(deltax)
-tmax = 0.5
+deltat = 0.125/4
+pt = [10.0, 10.125, 10.25,10.5,10.625,10.75,10.875,11.0]
+tmax = 12
 mu = b*deltat/deltax**2
 
+# uncomment for the time plot
+xplot = np.arange(0,L+deltax,deltax)
+
+# uncomment for the phase plot
+tplot = np.arange(0,tmax+2*deltat,deltat)
+yplot = np.empty((0,3))
+xloc = [10.,50.,120.]
 # Initialize variables
 a = np.arange(0,nx)*0.0
 b = np.arange(0,nx)*0.0
@@ -79,21 +86,17 @@ c = np.arange(0,nx)*0.0
 r = np.arange(0,nx)*0.0
 
 #BC Definition
-c1 = 0.0 # T at x = 0
+c1 = 1.0 # T at x = 0
 c2 = 0.0 # dT/dx at x = 0
 c3 = 0.0 # T at x = L
-c4 = 0.0 # dT/dx at x = L
-f1 = 1.0 # 
-f2 = 1.0 #
+c4 = 1.0 # dT/dx at x = L
 
 # IC definition
 x = np.arange(0,nx)*0.0
-x[0] = h0
 
-
-for t in np.arange(0,tmax,deltat):
+for t in tplot:
   # Changing boundary conditions from problem definition
-  c1 = h0*np.sin(2*np.pi*t/t0)
+  x[0] = h0*np.sin(2*np.pi*t/t0)
   
   # Determine a, b, c and r at the outset
   #a
@@ -110,12 +113,31 @@ for t in np.arange(0,tmax,deltat):
   c[0] = c2/deltax
   c[1:(nx-1)] = -mu
   c[nx-1] = 0
+  
+  J = len(b)
+  
+  # r
+  r[0] = c1*x[0] + c2*(x[1]-x[0])/deltax
+  r[1:(nx-1)] = x[1:(nx-1)]
+  r[nx-1] = c3*x[0] + c4*(x[J-1]-x[J-2])/deltax
+  
   ####### THOMAS ALGORITHM HERE
   # plug into Thomas
   x = thomas(a,b,c,r)
+  
+  # uncomment for time plot
+  #if t in pt:
+  #  axes.plot(xplot,x,color = [rand.random(),rand.random(),rand.random()],label = "t: " + str(t))
+  
+  # uncomment for phase plot
+  ypt = list()
+  for xl in xloc:
+     ypt = ypt + list(x[np.where(xplot==xl)])
+  yplot = np.vstack([yplot,ypt])
 
-xplot = np.arange(0,L+deltax,deltax)
-axes.plot(xplot,x,color = [rand.random(),rand.random(),rand.random()],label = "dt: " + str(deltat))
+for a in np.arange(0,3):
+      axes.plot(tplot,yplot[:,a],color = [rand.random(),rand.random(),rand.random()],label = "x: " + str(xloc[a]))
+
 
 ######## End Problem Part a
 ################################################

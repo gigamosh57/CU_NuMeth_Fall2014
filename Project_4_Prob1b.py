c9fc7c1f2562f6edea1b170e555dabe19e1df7f0
gigamosh57 <gigamosh57@gmail.com>
2014-11-14 23:12:01 -0700

    Update Project_4_Prob1b.py

--- Project_4_Prob1b.py
+++ Project_4_Prob1b.py
@@ -12,7 +12,6 @@ from datetime import datetime
 
 tol = 10**-6
 maxiter = 500000
-w = 1/4.
 
 # Grid size
 NX = 101
@@ -22,6 +21,9 @@ xmax = 1.
 ymax = 1.
 x = np.arange(0,xmax+xmax/(NX-1),xmax/(NX-1))
 y = np.arange(0,ymax+ymax/(NY-1),ymax/(NY-1))
+
+k=2.975
+w = 0.5*(1-k/NX)
 
 # initialize matrices
 u = np.empty((NX,NY))*0.
@@ -49,15 +51,15 @@ while err > tol:
   # Loop through interior nodes
   for i in range(1,NX-1):
     for j in range(1,NY-1):
-		  delta[i,j] = w*(u[i-1,j]+u[i,j-1]+u[i,j+1]+u[i+1,j]-4.*u[i,j])
-		  u[i,j] = u[i,j]+delta[i,j]
+        delta[i,j] = w*(u[i-1,j]+u[i,j-1]+u[i,j+1]+u[i+1,j]-4.*u[i,j])
+        u[i,j] = u[i,j]+delta[i,j]
   # calculate error
   err = np.max(delta)
   if it > maxiter: 
     err = tol
     print("Hit ",str(maxiter)," iter")
 
-print("For w = ",str(w)," SOR took ",str(it)," iterations")
+print("For k = ",str(k)," SOR took ",str(it)," iterations")
 
 ###### End GS/SOR solver
 ###################################

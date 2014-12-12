9dfa2386820e881dac4f83449b5676349eb0dd08
gigamosh57 <gigamosh57@gmail.com>
2014-12-11 18:52:19 -0700

    Create FinalProject_Prob1.py

---
+++ FinalProject_Prob1.py
@@ -0,0 +1,23 @@
+### Page Weil
+### CVEN 5537
+### 11/14/14
+### Final Project, Prob 1
+### 
+
+### Pseudocode
+
+# Load packages
+# Set up variables
+# p
+# initial conditions
+
+# time loop 
+  # create jacobian
+  # loop through evaluations of [F]
+    # solve with thomas
+    # update value
+    # check convergence
+  # Update u with converged values
+  # Store matrix of u values (time and space)
+  
+# plot multiple time values on single plot for single value of p

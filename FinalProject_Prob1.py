0bee32a9a8fad1a5c5a813977fd9b7a23be4f2ec
gigamosh57 <gigamosh57@gmail.com>
2014-12-12 11:22:36 -0700

    Update FinalProject_Prob1.py

--- FinalProject_Prob1.py
+++ FinalProject_Prob1.py
@@ -2,18 +2,120 @@
 ### CVEN 5537
 ### 11/14/14
 ### Final Project, Prob 1
-### 
 
-### Pseudocode
+import numpy as np
+# For pi
+import math
+# for plotting
+import matplotlib.pylab as plt
+# for colors
+import random as rand
+# for timing
+from datetime import datetime
 
-# Load packages
-# Set up variables
-# p
-# initial conditions
+################################################
+######## Thomas Solver
+
+def thomas(a,b,c,r):
+   # Define vectors as size of b
+   e = b*0
+   f = b*0
+   g = b*0
+   y = b*0
+   J = len(b)
+   
+   # define efg matrix
+   f[0] = b[0]
+   g[0] = c[0]/f[0]
+   for i in np.arange(1,J):
+      e[i] = a[i]
+      f[i] = b[i]-e[i]*g[i-1]
+      g[i] = c[i]/f[i]
+   
+   # solve for y
+   y[0] = r[0]/f[0] 
+   for i in np.arange(1,J):
+      y[i] = (r[i]-e[i]*y[i-1])/f[i]
+   
+   # solve for x
+   x[J-1] = y[J-1] 
+   for i in np.arange(0,J-1)[::-1]:
+      x[i] = (y[i]-g[i]*x[i+1])
+   return x
+
+######## End Thomas Solver
+################################################
+
+
+###################################
+###### Begin define problem variables
+
+tol = 10**-6
+maxiter = 100
+
+# Grid size
+dx = 0.01
+dt = 0.01
+xmax = 1
+tmax = 100
+
+NX = int(xmax/dx)+1
+NT = int(tmax/dt)+1
+
+x = np.arange(0,xmax+dx,dx)
+t = np.arange(0,tmax+dt,dt)
+
+alpha = 1.
+p = 0 # 0,1,2,4
+
+mu = alpha*dt/dx**2
+
+# Boundary conditions
+c1 = 1
+c2 = 0
+c3 = 1
+c4 = 0
+b1 = 0
+bj = 1
+
+# initialize matrices and initial conditions 
+u = np.zeros((NX,NT)) # starts at zero everywhere except the end
+u[NX-1,:]=bj
+unew = np.zeros((NX)) 
+uold = np.zeros((NX))  
+a = np.zeros((NX)) 
+a[NX-1]=-c4/dx
+b = np.zeros((NX)) 
+
+c = np.zeros((NX)) 
+f = np.zeros((NX)) 
+
+###### End define problem variables
+###################################
 
 # time loop 
-  # create jacobian
+for ti in range(0,NT):
+
+unew = u[:,ti]
+uold = u[:,ti]
   # loop through evaluations of [F]
+  # evaluate f
+f[0] = (c1-c2/dx)*unew[0]+(c2/dx*unew[1])-b1
+f[1:(NX-2)] = -mu/(p+1)*(unew[0:NX-3]**(p+1))+(unew[1:NX-2]+2*mu/(p+1)*unew[1:NX-2]**(p+1))-mu/(p+1)*(unew[2:NX-1]**(p+1))-uold[1:NX-2]
+f[NX-1] = (-c4/dx)*unew[NX-2]+(c3+c4/dx*unew[NX-1])-bj
+
+  # create jacobian (as abc)
+a = 
+b = 
+c = 
+
+
+
+    # evaluate f
+
+    # calculate a,b,c
+
+delu = thomas(a,b,c,-1*f)
     # solve with thomas
     # update value
     # check convergence
